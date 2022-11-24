8d17abfc28911fa96972b79f42148515e6229597
MadeleineWalder <madeleinezoewalder@gmail.com>
2022-11-24 14:12:10 +0000

    Add code to calculate two random numbers for the game question

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+// Docstrings are used to describe functions, they should go right above where the function name is declared. 
+// They are like comments but when you hover your mouse over the function name (no matter where it is used or how many times) it will show you the comment.
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+
+    // Creates two random nunbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+
+
+
+
+
+runGame()
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
