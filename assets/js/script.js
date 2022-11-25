fa7487b1bb668c24d93061117dd1cc9faafaef9a
MadeleineWalder <madeleinezoewalder@gmail.com>
2022-11-25 10:15:20 +0000

    Display addition to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 // Docstrings are used to describe functions, they should go right above where the function name is declared. 
@@ -23,11 +26,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random nunbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -46,8 +56,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubractQuestion() {
@@ -55,5 +67,9 @@ function displaySubractQuestion() {
 }
 
 function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
 
 }
